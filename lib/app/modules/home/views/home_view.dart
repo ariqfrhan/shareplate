a268b9c802ab34a8332f76f2bb89879f38d02951
ariqfrhan <muhammadariq3@gmail.com>
2024-01-12 23:00:20 +0700

    `refactor` : splash, onboarding, login sequence

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:shareplate/app/utils/shareplate_colors.dart';
 
 import '../controllers/home_controller.dart';
 
@@ -9,7 +10,7 @@ class HomeView extends GetView<HomeController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('HomeView'),
+        title: const Text('Shareplate', style: TextStyle(color: ShareplateColor.primary, fontFamily: 'Amaranth'),),
         centerTitle: true,
       ),
       body: const Center(
